565903f1cd3e5c428628035cc63e4d17d4cf8ab1
Amit Gupta <amigup@microsoft.com>
2020-12-06 13:45:11 +0530

    Added service bus and timer triggered implementation

---
+++ CleanArchitecture.Core/Interfaces/IMessageService.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Shared.Contracts;
+using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Core.Interfaces
+{
+	public interface IMessageService
+	{
+		Task<TableEntity> InsertOrMergeAsync<T>(T record) where T : IMessageTable, ITableEntity;
+
+		Task CleanupAsync(string entityName, string partition);
+	}
+}
