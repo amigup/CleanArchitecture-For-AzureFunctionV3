0df9704f6222ac833669c95ecabdf8360263fd06
Amit Gupta <amigup@microsoft.com>
2020-12-06 15:33:50 +0530

    Added ignored classes

---
+++ CleanArchitecture.Infrastructure/Providers/PartitionKeyProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CleanArchitecture.Infrastructure.Providers
+{
+	public static class PartitionKeyProvider
+	{
+		public static string GetPartitionKey(DateTime dateTime)
+		{
+			return dateTime.ToString("yyyy-MM-dd");
+		}
+	}
+}
---
+++ CleanArchitecture.Shared/Models/Requests/MessageRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CleanArchitecture.Shared.Models.Requests
+{
+	public class MessageRequest
+	{
+		public Guid Id { get; set; }
+
+		public string Message { get; set; }
+	}
+}
